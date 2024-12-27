71ec79b6a217220f6fecf12581b0b0b02bdb460f
Jett Drafahl <jettdrafahl@gmail.com>
2024-12-26 19:16:22 -0600

    update

--- src/main/java/com/devlink/devlink/controller/SkillController.java
+++ src/main/java/com/devlink/devlink/controller/SkillController.java
@@ -1,9 +1,11 @@
 package com.devlink.devlink.controller;
 
 import com.devlink.devlink.dao.SkillDao;
+import com.devlink.devlink.model.Skill;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/skills")
@@ -14,5 +16,36 @@ public class SkillController {
     @Autowired
     public SkillController(SkillDao skillDao) {
         this.skillDao = skillDao;
+    }
+
+
+    @GetMapping("/{id}")
+    public Skill getSkillById(@PathVariable Long id) {
+        return skillDao.getSkillById(id);
+    }
+
+    @GetMapping("/user_id")
+    public List<Skill> getSkillsByUserId(@PathVariable Long userId) {
+        return skillDao.getSkillsByUserId(userId);
+    }
+
+    @GetMapping
+    public List<Skill> getAllSkills() {
+        return skillDao.getAllSkills();
+    }
+
+    @PostMapping
+    public Long createSkill(@RequestBody Skill skill) {
+        return skillDao.createSkill(skill);
+    }
+
+    @PutMapping("/{id}")
+    public boolean updateSkill(@PathVariable Long id, @RequestBody Skill skill) {
+        return skillDao.updateSkill(id, skill);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteSkill(@PathVariable Long id) {
+        return skillDao.deleteSkill(id);
     }
 }
