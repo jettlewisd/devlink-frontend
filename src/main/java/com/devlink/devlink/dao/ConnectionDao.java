2bce5debdd5f107dff16aa839dd4aa311a844e96
Jett Drafahl <jettdrafahl@gmail.com>
2024-12-28 18:55:29 -0600

    test

--- src/main/java/com/devlink/devlink/dao/ConnectionDao.java
+++ src/main/java/com/devlink/devlink/dao/ConnectionDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 public interface ConnectionDao {
 
     // CRUD OPS
-    Connection getConnection(Long userId, Long connectedUserId);
+    Connection getConnection(Long userId, Long connectedUserId); 
     List<Connection> getAllConnections();
     boolean createConnection(Connection connection);
     boolean removeConnection(Long userId, Long connectedUserId);
