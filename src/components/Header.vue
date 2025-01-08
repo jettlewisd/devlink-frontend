b56375537014e98ec13d39b6b45a583b50d1b7e1
Jett Drafahl <jettdrafahl@gmail.com>
2025-01-08 11:58:11 -0600

    component update

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <!-- Placeholder Content -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ComponentName', // Replace with the component's name
+  };
+  </script>
+  
+  <style scoped>
+  /* Add styles here later */
+  </style>
+  
