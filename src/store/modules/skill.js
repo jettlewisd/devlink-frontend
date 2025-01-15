b92a8f0101f3e70a070d269ab18a2856354451ca
Jett Drafahl <jettdrafahl@gmail.com>
2025-01-15 13:17:09 -0600

    store module updates

---
+++ src/store/modules/skill.js
@@ -0,0 +1,34 @@
+import api from '@/api'; //import axios instance from api.js 
+
+const state = {
+    skills: [], 
+};
+
+const getters = {
+    allSkills: (state) => state.skills, //getter to get all skills
+};
+
+const actions = {
+    async fetchSkills( {commit} ) {
+        try {
+            const response = await api.get('/skills');
+            commit('setSkills', response.data);
+        } catch (error) {
+            console.error("Error fetching skills:", error);
+        }
+    },
+};
+
+const mutations = {
+    setSkills(state, skills) {
+        state.skills = skills;
+    },
+};
+
+export default {
+    namespaced: true,
+    state, 
+    getters,
+    actions,
+    mutations,
+};
---
+++ src/store/modules/user.js
@@ -0,0 +1,35 @@
+import api from '@/api'; //import axios instance from api.js 
+
+const state = {
+    users: [], 
+};
+
+const getters = {
+    allUsers: (state) => state.users, // gettter to get all users
+};
+
+const actions = {
+    async fetchUsers( {commit } ) {
+        try {
+            const response = await api.get('/users');  // API call to fetch users
+            commit('setUsers', response.data);  // commit the data to the store 
+        } catch (error) {
+            console.error("Error fetching users:", error);  // handle any errors during the request
+        }
+    },
+};
+
+const mutations = {
+    setUsers(state, users) {
+        state.users = users;  // Set the users in the state 
+    },
+};
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
+};
+
