db9a1acad5bf340ff02c0e22976101715b86e77b
Jett Drafahl <jettdrafahl@gmail.com>
2025-01-17 17:31:56 -0600

    componentssss

---
+++ src/components/CreateProjectForm.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="create-project-form">
+        <h3>Create a New Project</h3>
+    </div>
+
+    <!-- project name input -->
+
+
+    <!-- project description input -->
+
+
+    <!-- Start Date -->
+
+
+    <!-- end date -->
+
+
+    <!-- submit button -->
+     <button @click="submitProject">Create Project</button>
+
+</template>
+
+
+
+
+
+
+
+
+
+
+
+<script>
+export default {
+
+}
+
+</script>
+
+
+
+
+
+
+
+
+
+
+<style>
+
+</style>
