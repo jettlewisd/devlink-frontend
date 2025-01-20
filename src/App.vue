310a062ebee0b697941d1864caa4d218ae4cfb19
Jett Drafahl <jettdrafahl@gmail.com>
2025-01-20 17:17:40 -0600

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <!-- Optional Header -->
+    <AppHeader />
+    
+    <!-- Main Content: Routed Views -->
+    <router-view />
+
+    <!-- Optional Footer -->
+    <AppFooter />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from '@/components/AppHeader.vue';
+import AppFooter from '@/components/AppFooter.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    AppHeader,
+    AppFooter,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* You can add global styles here if needed */
 </style>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="projects-view">
-      <AppHeader /> <!-- Include the AppHeader -->
-      
-      <!-- Add a section to show all projects -->
-      <h2>Projects</h2>
-      <ProjectList /> <!-- The ProjectList component will display the projects -->
+    <AppHeader /> <!-- Include the AppHeader -->
 
-      <AppFooter /> <!-- Include the AppFooter -->
+    <!-- Add a section to show all projects -->
+    <h2>Projects</h2>
+    <!-- Show a message if no projects are available -->
+    <ProjectList /> <!-- The ProjectList component will display the projects -->
+
+    <AppFooter /> <!-- Include the AppFooter -->
   </div>
 </template>
 
@@ -15,19 +16,24 @@
 import AppHeader from '@/components/AppHeader.vue';
 import AppFooter from '@/components/AppFooter.vue';
 import ProjectList from '@/components/ProjectList.vue';
+import { mapActions } from 'vuex'; // mapActions to dispatch actions
 
 export default {
   name: 'ProjectsView',
   
   components: {
-      AppHeader,
-      AppFooter,
-      ProjectList,
+    AppHeader,
+    AppFooter,
+    ProjectList,
   },
 
   created() {
-      // Dispatch the action to fetch projects when the component is created
-      this.$store.dispatch('project/fetchProjects');
+    // Dispatch the action to fetch projects when the component is created
+    this.fetchProjects();
+  },
+
+  methods: {
+    ...mapActions('projects', ['fetchProjects']), // Map the fetchProjects action from the store
   },
 };
 </script>
