547ce73a6c7fabeeacd52dc3c0547d901c9bea26
Jett Drafahl <jettdrafahl@gmail.com>
2025-01-25 12:15:56 -0600

    update

--- src/services/SkillService.js
+++ src/services/SkillService.js
@@ -9,4 +9,8 @@ const SkillService = {
     fetchSkillById(id) {
         return api.get(`/skills/${id}`); // NOTE: use backticks in the param when performing interpolation 
     },
+
+    fetchSkillsByUserId(id) {
+        return api.get(`/skills/${userId}`); // NOTE: use backticks in the param when performing interpolation 
+    },
 };
--- src/store/modules/skill.js
+++ src/store/modules/skill.js
@@ -4,12 +4,14 @@ import SKillService from '@/services/SkillService'
 const state = {
     skills: [],
     selectedSkill: null,
+    selectedSkills: null,
     loading: false,
 }
 
 const getters = {
     allSKills: (state) => state.skills, // get all skills 
     getSelectedSkill: (state) => state.selectedSkill, // get selected skill 
+    getSelectedSkills: (state) => state.selectedSkills // get selected skill(s)
 };
 
 const mutations = {
@@ -18,6 +20,9 @@ const mutations = {
     },
     setSelectedSkill(state, selectedSkill) {
         state.selectedSkill = selectedSkill;
+    },
+    setSelectedSkills(state, selectedSkills) {
+        state.selectedSkills = selectedSkills;
     },
     setLoading(state, loading) {
         state.loading = loading;
@@ -40,12 +45,12 @@ const actions = {
         }
     },
 
-    async fetchSkillsById({commit}) {
+    async fetchSkillById({commit}, id) {
         try {
             commit('setLoading', true);
-            const response = await SKillService.fetchSkillsById(id);
+            const response = await SKillService.fetchSkillById(id);
             console.log('API response:', response.data);
-            commit('setSelectedProject', response.data);
+            commit('setSelectedSkill', response.data);
         } catch (error) {
             console.log('Error fetching skill by ID', error);
             throw error;
@@ -54,5 +59,52 @@ const actions = {
         }
     },
 
-    async 
+    async fetchSkillsByUserId({commit}, userId) {
+        try {
+            commit('setLoading', true);
+            const response = await SKillService.fetchSkillsByUserId(userId);
+            console.log('API response:', response.data);
+            commit('setSelectedSkills', response.data);
+        } catch (error) {
+            console.log('Error fetching skills by User ID', error);
+            throw error;
+        } finally {
+            commit('setLoading', false);
+        }
+    },
+
+    async createSkill({dispatch}, skill) {
+        try {
+            console.log('Creating skill with data:', skill);
+            await SKillService.createSkill(skill);
+            await dispatch('fetchSkills');
+        } catch (error) {
+            console.log('Error creating skill:', error);
+            throw error;
+        }
+    },
+
+    async updateSkill({dispatch}, {id, skill}) {
+        try {
+            await SKillService.updateSkill(id, skill);
+            await dispatch('fetchSkills');
+        } catch (error) {
+            console.log('Error updating skill:', error);
+            throw error;
+        }
+    },
+
+    async deleteSkill({dispatch}, id) {
+        try {
+            await SKillService.deleteSkill(id);
+            await dispatch('fetchSkills');
+        } catch (error) {
+            console.log('Error deleting skill:', error);
+            throw error;
+        }
+    },
+
+    async fetc
+
+    
 };
